1ce71e2de10e2787cec49d8765574baa4d283d78
a14a19 <anas31197@gmail.com>
2022-11-09 21:07:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Navbar from './Components/navbar';
 import Home from './Components/home';
+import AboutView from './Components/about';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/about' element={<AboutView />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/about.js
@@ -0,0 +1,11 @@
+const AboutView = () =>{
+    return(
+        <>
+            <h3>
+                About Us
+            </h3>
+        </>
+    )
+}
+
+export default AboutView;
